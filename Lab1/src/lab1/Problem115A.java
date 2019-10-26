a99887a99f779401a853a363c19c3e78b511a9e8
florekkinga <florek.kinga@gmail.com>
2019-10-26 22:47:37 +0200

    added Lab2

--- Lab1/src/lab1/Problem115A.java
+++ Lab1/src/lab1/Problem115A.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Problem115A {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę pracowników");
         int n = scan.nextInt();
 
         int[] arr = new int[n + 1];
+        arr[0] = 0;
         int managed;
         int result = 0;
 
         for (int i = 1; i <= n; i++) {
+            System.out.printf("Podaj managera pracownika %d\n", i);
             arr[i] = scan.nextInt();
 
             managed = 0;
@@ -19,8 +22,9 @@ public class Problem115A {
                 manager = arr[manager];
                 managed += 1;
             }
-
             result = Math.max(result, managed);
+            System.out.println(result);
+
         }
         System.out.println(result + 1);
     }
