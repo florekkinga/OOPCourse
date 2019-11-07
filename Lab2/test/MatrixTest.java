f7981a9883b837b521dc80794366ada60649c81f
florekkinga <florek.kinga@gmail.com>
2019-11-08 00:53:18 +0100

    added unit tests for frobenius method

--- Lab2/test/MatrixTest.java
+++ Lab2/test/MatrixTest.java
@@ -193,12 +193,10 @@ public class MatrixTest {
 
     @org.junit.Test
     public void frobenius() {
+        Matrix m = new Matrix(new double [][] {{2,2},{1,1}});
+        assertEquals(10, m.frobenius(), 0.000001);
     }
-
-    @org.junit.Test
-    public void random() {
-    }
-
+    
     @org.junit.Test
     public void eye() {
         Matrix m = Matrix.eye(10);
